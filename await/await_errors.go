8e80bf615cba94fa771afec747604e5975b6c013
n8wb <vanish172@gmail.com>
2020-01-18 12:34:56 -0800

    add error awaiter

---
+++ await/await_errors.go
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2019 Innodev LLC. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+package await
+
+import (
+	"github.com/innodv/errors"
+)
+
+func AwaitErrors(errChan <-chan error, count int) error {
+	var err error
+	for i := 0; i < count; i++ {
+		err2 := <-errChan
+		if err2 != nil {
+			if err == nil {
+				err = err2
+			} else {
+				err = errors.Wrap(err, err2.Error())
+			}
+		}
+	}
+	return err
+}
